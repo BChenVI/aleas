1eb0b1e69d3080528248f194940ef75a074bb86c
benchi <dsqd@gmail.com>
2015-04-07 15:25:31 +0200

    ajout 6

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	error_reporting(E_ALL);
-	ini_set
+	ini_set("display_errors",1);
+	session_start();
+	
 	require "facebook-php-sdk-v4-4.0-dev/autoload.php"
 	
 	const APPID = "1431368163839906"
