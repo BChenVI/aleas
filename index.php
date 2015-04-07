c875ae20b88c5a38d58c062064de6d14e65e58e5
benchi <dsqd@gmail.com>
2015-04-07 15:09:28 +0200

    ajout bouton like

--- index.php
+++ index.php
@@ -24,11 +24,19 @@
 		 var js, fjs = d.getElementsByTagName(s)[0];
 		 if (d.getElementById(id)) {return;}
 		 js = d.createElement(s); js.id = id;
-		 js.src = "//connect.facebook.net/en_US/sdk.js";
+		 js.src = "//connect.facebook.net/fr_FR/sdk.js";
 		 fjs.parentNode.insertBefore(js, fjs);
 	   }(document, 'script', 'facebook-jssdk'));
 	</script>
 	<h1> TITRE 1 </h1>
+	
+	<div
+	  class="fb-like"
+	  data-share="true"
+	  data-width="450"
+	  data-show-faces="true">
+	</div>
+	
 </body>
 
 </html> 
